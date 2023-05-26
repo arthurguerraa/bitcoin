5a24c11b59d57b40bc5c21599b7036247d198196
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2023-05-26 13:25:10 -0300

    Initial commit

---
+++ bitcoin/lib/bitcoin.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Bitcoin extends StatefulWidget {
+
+ const Bitcoin({super.key});
+  @override
+  State<Bitcoin> createState() => _BitcoinState();
+}
+
+class _BitcoinState extends State<Bitcoin> {
+
+  String _preco = "0";
+
+  void _recuperarPreco() async{
+
+    var url = Uri.parse("https://blockchain.info/ticker");
+
+    http.Response reponse = await http.get(url);
+    Map<String, dynamic> retorno = json.decode(reponse.body);
+    setState(() {
+      _preco = retorno["BRL"] ["buy"].toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("Imagens/bitcoin.png"),
+              SizedBox(height: 20,),
+              Text("R\$ $_preco ", style: TextStyle(fontSize: 35),),
+              SizedBox(height: 20,),
+              TextButton(onPressed: (){_recuperarPreco();},
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orange)),
+                child: const Text("Atualizar", style: TextStyle(fontSize: 20, color: Colors.white),)
+                )
+            ],
+          )
+    ),
+  ),
+);
+}
+}
+
---
+++ bitcoin/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'bitcoin.dart';
+void main() {
+  runApp(MaterialApp(
+    home: Bitcoin(),
+  ));
+}
+
